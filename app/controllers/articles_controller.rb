6800358cdfaa8eebcd7ed56d9cc36d41298e5354
jubaan <julio.ab@outlook.com>
2020-04-24 11:57:35 -0500

    Add: form and create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,10 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create; end
 end
