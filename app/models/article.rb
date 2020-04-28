3436742f1192bb3267be2ec49ed4c19178c19033
fc-anjos <felipe.anjos@usp.br>
2020-04-28 18:14:35 -0300

    FIX: Deletes tags when deleting article

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,7 +13,7 @@ end
 
   has_many :comments
   has_many :taggings
-  has_many :tags, through: :taggings
+  has_many :tags, through: :taggings, dependent: :destroy
 
   has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
   validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
