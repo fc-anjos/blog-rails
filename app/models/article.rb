f25aedbb0f45003b78e5a5e3ff01159c03f567da
jubaan <julio.ab@outlook.com>
2020-04-28 14:30:27 -0500

    Create: database with paperclip gem, Update: article model and helper to accept and validate images

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,4 +14,7 @@ end
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
