1cfe6c06f0d1c1b85e4c213285481272fdca013a
fc-anjos <felipe.anjos@usp.br>
2020-04-28 16:54:41 -0300

    ADD: Display imagens on articles, Resize images through Imagemagick

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,6 +15,6 @@ end
   has_many :taggings
   has_many :tags, through: :taggings
 
-  has_attached_file :image
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
+  validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
 end
